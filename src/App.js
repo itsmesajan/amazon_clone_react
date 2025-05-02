a6e7a4e35c9aa0733e4fb4fab36fb13c0b0012a1
Sajan Maharjan <itsmesajanmaharjan@gmail.com>
2025-05-02 09:31:04 +0545

    deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './Home';
 import Header from './Header';
 import CheckOut from './CheckOut';
